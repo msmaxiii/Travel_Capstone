5edac52fa7e9b332c91245224cb30074968fb2ab
msmaxiii <maxwellrg@gmail.com>
2022-09-07 14:07:40 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// const  express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+
+// app.use(cors());
+// app.use(express.json());
+
+// // console.log('app created')
+
+const {
+    getCountry,
+    getDestination,
+    postItem,
+    deleteItem
+} = require("./controller"):
+
+// app.get
+// app.post
+// app.delete
+
+
+
+
+// const PORT = 8080;
+// app.listen(PORT,()=>
+//     console.log(`I'm here listening on ${PORT}`)
+// );
